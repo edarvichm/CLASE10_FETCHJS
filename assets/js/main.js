eebe5f7f354d7fd188379ed05d02bd4c8364ef48
edarvichm <edarvichm@gmail.com>
2023-04-22 22:09:39 -0400

    codigo con paginacion

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,7 @@
 // agregar <p> al html con el id="pokemnDitto"
 const pokemon = document.getElementById('pokemon');
 const pokemonList = document.getElementById('pokemonList');
+const pokemons = []; 
 // console.log(pokemonDitto);
 // fetch
 function muestraPokemon(url) {
@@ -42,7 +43,8 @@ const getData = async () => {
     const data = await respuesta.json()
     console.log(data)
     for (let i = 0; i < data.results.length; i++) {
-        imprimePokemon(data.results[i])
+        // imprimePokemon(data.results[i])
+        pokemons.push(data.results[i]);
     }
     pokemon.removeChild(document.getElementById('cargando'))
     pokemon.innerHTML = `
@@ -58,3 +60,55 @@ getData()
 // async function ejemplo(){
 
 // }
+
+
+const itemsPerPage = 20;
+let currentPage = 1;
+let currentSearch = '';
+
+function generatePokemonList(search = '') {
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const filteredPokemons = search
+        ? pokemons.filter(pokemon => pokemon.name.toLowerCase().includes(search.toLowerCase()))
+        : pokemons;
+
+    const pagePokemons = filteredPokemons.slice(startIndex, startIndex + itemsPerPage);
+
+    const pokemonHTML = pagePokemons.map(pokemon => `
+        <div class="col-md-6">
+            <p>${pokemon.name} - <button onclick="muestraPokemon('${pokemon.url}')">Ver</button></p>
+        </div>
+    `).join('');
+
+    document.getElementById('pokemonList').innerHTML = pokemonHTML;
+
+    generatePagination(filteredPokemons.length);
+}
+
+function generatePagination(totalItems) {
+    const totalPages = Math.ceil(totalItems / itemsPerPage);
+    let paginationHTML = '';
+
+    for (let i = 1; i <= totalPages; i++) {
+        paginationHTML += `
+            <li class="page-item${i === currentPage ? ' active' : ''}">
+                <a class="page-link" href="#" onclick="changePage(${i})">${i}</a>
+            </li>
+        `;
+    }
+
+    document.getElementById('pagination').innerHTML = paginationHTML;
+}
+
+function changePage(newPage) {
+    currentPage = newPage;
+    generatePokemonList(currentSearch);
+}
+
+document.getElementById('searchInput').addEventListener('input', (event) => {
+    currentSearch = event.target.value;
+    currentPage = 1;
+    generatePokemonList(currentSearch);
+});
+
+generatePokemonList();
